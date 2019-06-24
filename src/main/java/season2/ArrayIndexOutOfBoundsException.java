0ad5b1f613a1f77841679092bb136fdcd10d74d2
dohuunghia11 <huunghiado1995@gmail.com>
2019-06-24 17:02:20 +0700

    ss2

--- src/main/java/season2/ArrayIndexOutOfBoundsException.java
+++ src/main/java/season2/ArrayIndexOutOfBoundsException.java
@@ -1,16 +1,17 @@
 package season2;
 
-import season1.CheckPrime;
+import java.util.Scanner;
 
 public class ArrayIndexOutOfBoundsException {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Nhap so phan tu cua mang");
-        int length = CheckPrime.scanner.nextInt();
+        int length = scanner.nextInt();
         System.out.println("Nhap cac phan tu trong mang");
         int[] arr = InsertArray.importArrayOneWay(length);
         System.out.println("Vui lòng nhập chỉ số của một phần tử bất kỳ: ");
-        int x = CheckPrime.scanner.nextInt();
+        int x = scanner.nextInt();
         try {
             System.out.println("Giá trị của phần tử có chỉ số 5" + x + " là " + arr[x]);
         } catch (IndexOutOfBoundsException e) {
